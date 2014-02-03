747034be28468b6de9d38bc2eef6975705940194
Pier Paolo Ramon <ramonpierre@gmail.com>
2014-02-03 13:04:13 +0100

    Test for building phase

--- test/packer/index.js
+++ test/packer/index.js
@@ -1,5 +1,6 @@
 
 var vm = require('vm');
+var sequence = require('when/sequence');
 var path = require('path');
 var packer = require('../../packer');
 var fs = require('../../lib/util/fs');
@@ -14,25 +15,45 @@ var Titanium = {
 	}
 };
 
-fs.mkdirp(path.resolve(__dirname, 'build'))
-.then(function () {
-	return packer.pack(path.resolve(__dirname, 'fake-module'), {
-		//
+var BUILD_DIR = path.resolve(__dirname, 'build');
+var ENTRY = path.resolve(__dirname, 'fake-module');
+
+sequence([
+	testPack,
+	testBuild.bind(null, {}),
+	testBuild.bind(null, {
+		noDependencies: true
+	})
+]).done();
+
+function testPack() {
+	return fs.mkdirp(BUILD_DIR)
+	.then(function () {
+		return packer.pack(ENTRY, {
+			//
+		});
+	})
+	.then(function (src) {
+		return fs.writeFile(path.resolve(BUILD_DIR, 'packed.js'), src).yield(src);
+	})
+	.then(function (src) {
+		var sandbox = {
+			Titanium: Titanium,
+			Ti: Titanium,
+			module: {
+				exports: {}
+			}
+		};
+
+		sandbox.exports = sandbox.module.exports;
+
+		vm.runInNewContext(src, sandbox, require.resolve('./fake-module'));
 	});
-})
-.then(function (src) {
-	return fs.writeFile(path.resolve(__dirname, 'build', 'packed.js'), src).yield(src);
-})
-.done(function (src) {
-	var sandbox = {
-		Titanium: Titanium,
-		Ti: Titanium,
-		module: {
-			exports: {}
-		}
-	};
+}
 
-	sandbox.exports = sandbox.module.exports;
-
-	vm.runInNewContext(src, sandbox, require.resolve('./fake-module'));
-});
+function testBuild(cfg) {
+	cfg.entry = ENTRY;
+	return packer.build(cfg).then(function (zip) {
+		return zip.writeModule(BUILD_DIR);
+	});
+}
