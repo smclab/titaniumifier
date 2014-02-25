61b8d48f2c69c9624f9fa3a079c2d26f9c571908
Pier Paolo Ramon <ramonpierre@gmail.com>
2014-02-25 13:39:06 +0100

    Failing tests for renaming feature

--- test/packer/index.js
+++ test/packer/index.js
@@ -1,5 +1,8 @@
 
+require('colors');
+
 var vm = require('vm');
+var when = require('when');
 var sequence = require('when/sequence');
 var path = require('path');
 var packer = require('../../packer');
@@ -23,8 +26,19 @@ sequence([
   testBuild.bind(null, {}),
   testBuild.bind(null, {
     noDependencies: true
-  })
-]).done();
+  }),
+  testBuild.bind(null, {
+    as: 'fake-name'
+  }),
+  testBuild.bind(null, {
+    as: 'fake-name',
+    noDependencies: true
+  }),
+  testZips,
+]).done(function () {
+  console.log('');
+  console.log(' ✔ '.green + "Packer tests run smootly");
+});
 
 function testPack() {
   return fs.mkdirp(BUILD_DIR)
@@ -55,5 +69,24 @@ function testBuild(cfg) {
   cfg.entry = ENTRY;
   return packer.build(cfg).then(function (zip) {
     return zip.writeModule(BUILD_DIR);
+  });
+}
+
+function testZips() {
+  return fs.readdir(BUILD_DIR).then(function (files) {
+    if (files.indexOf("fake-module-commonjs-1.2.3-bare.zip") < 0) {
+      throw new Error("No bare zip built");
+    }
+    if (files.indexOf("fake-module-commonjs-1.2.3.zip") < 0) {
+      throw new Error("No simple zip built");
+    }
+    if (files.indexOf("fake-name-commonjs-1.2.3-bare.zip") < 0) {
+      throw new Error("No bare zip (with fake name) built");
+    }
+    if (files.indexOf("fake-name-commonjs-1.2.3.zip") < 0) {
+      throw new Error("No simple zip (with fake name) built");
+    }
+
+    console.log(' ✔ '.green + "All (4) zips built correctly");
   });
 }
