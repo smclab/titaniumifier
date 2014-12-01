b864d92ce2abec4b9f5e5c7b4a08ae1449ccba73
Roberto Zaratin <roberto.zaratin@smc.it>
2014-12-01 18:41:33 +0100

    Initial porting to mocha

--- test/manifest/index.js
+++ test/manifest/index.js
@@ -2,7 +2,9 @@
 var manifest = require('../../manifest');
 
 require('colors');
+
 var diff = require('diff');
+var should = require('should');
 
 var path = require('path');
 var fs = require('fs');
@@ -20,94 +22,82 @@ var cfgs = readdir(fixturesDir).sort().map(function (filename) {
 	};
 });
 
-cfgs.forEach(function (cfg) {
-	var invalidPackage = cfg.filename.indexOf('INVALID') >= 0;
+describe("Cases:", function () {
+	cfgs.forEach(function (cfg) {
+		it(cfg.filename, function () {
+			var invalidPackage = cfg.filename.indexOf('INVALID') >= 0;
 
-	function ok(msg) {
-		console.log(' ✔ '.green + cfg.filename.white);
-		if (msg) {
-			console.log(' · '.green + msg.blue);
-		}
-	}
-
-	function notOk() {
-		console.log(' ✘ '.red + cfg.filename.yellow);
-	}
-
-	var built = false;
-
-	try {
-		manifest.validatePackage(cfg.package);
-
-		built = true;
-
-		if (invalidPackage) {
-			throw new Error("Building for " + cfg.filename + " should throw");
-		}
-
-		var expectedFilename = cfg.fullFilename
-			.replace('fixtures', 'expected')
-			.replace('package', 'manifest')
-			.replace('.json', '');
-
-		var result = manifest.buildFromPackage(cfg.package).trim();
-
-		var expected = readFile(expectedFilename, 'utf8').trim()
-			.replace('$YEAR$', new Date().getFullYear());
-
-		var err;
-
-		if (result !== expected) {
-
-			err = new Error("Wrong results for " + cfg.filename);
-			err.data = [];
-
-			err.data.push("\n");
-			err.data.push(cfg.filename.green + "\n");
-			err.data.push(path.relative(__dirname, expectedFilename).red + "\n");
-			err.data.push("\n");
-
-			diff.diffLines(expected, result).forEach(function(part){
-				// green for additions, red for deletions
-				// grey for common parts
-				var color =
-					part.added ? 'green' :
-					part.removed ? 'red' :
-					'white';
-
-				if (part.added || part.removed) {
-					err.data.push(expose(part.value, color));
-				}
-				else {
-					err.data.push(part.value[color]);
-				}
-			});
-
-			err.data.push("\n");
-			err.data.push("\n");
-
-			throw err;
-		}
-
-		ok();
-	}
-	catch (err) {
-		if (invalidPackage && !built) {
-			ok(err.message ? ('Correctly throws: ' + err.message) : 'Correctly throws error');
-			return;
-		}
-		else {
-			notOk();
-
-			if (err.data) {
-				process.stderr.write(err.data.join(''));
+			if (invalidPackage) {
+				(function () {
+					build(cfg.filename, cfg.fullFilename, cfg.package, invalidPackage);
+				}).should.throw();
 			}
 			else {
-				throw err;
+				build(cfg.filename, cfg.fullFilename, cfg.package, invalidPackage);
 			}
-		}
-	}
+		});
+	});
 });
+
+function build(filename, fullFilename, pkg, invalidPackage) {
+
+	manifest.validatePackage(pkg);
+
+	if (invalidPackage) {
+		return;
+	}
+
+	var expectedFilename = fullFilename
+	.replace('fixtures', 'expected')
+	.replace('package', 'manifest')
+	.replace('.json', '');
+
+	var result = manifest.buildFromPackage(pkg).trim();
+
+	var expected = readFile(expectedFilename, 'utf8').trim()
+	.replace('$YEAR$', new Date().getFullYear());
+
+	var resultLines = result.split('\n');
+	var expectedLines = expected.split('\n');
+
+	resultLines.forEach(function (line, i) {
+		line.should.be.eql(expectedLines[i]);
+	});
+
+	/*var err;
+
+	if (result !== expected) {
+
+		err = new Error("Wrong results for " + filename);
+		err.data = [];
+
+		err.data.push("\n");
+		err.data.push(filename.green + "\n");
+		err.data.push(path.relative(__dirname, expectedFilename).red + "\n");
+		err.data.push("\n");
+
+		diff.diffLines(expected, result).forEach(function(part){
+			// green for additions, red for deletions
+			// grey for common parts
+			var color =
+			part.added ? 'green' :
+			part.removed ? 'red' :
+			'white';
+
+			if (part.added || part.removed) {
+				err.data.push(expose(part.value, color));
+			}
+			else {
+				err.data.push(part.value[color]);
+			}
+		});
+
+		err.data.push("\n");
+		err.data.push("\n");
+
+		throw err;
+	}*/
+}
 
 function expose(line, color) {
 	line = replace(line, ' ', '·', color);
@@ -125,5 +115,3 @@ function replace(line, from, to, color) {
 		}
 	}).join('');
 }
-
-console.log('');
