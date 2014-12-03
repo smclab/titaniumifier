9230c08b50d7686d2c63b1c0ae5631229489c6f2
yuchi <ramonpierre@gmail.com>
2014-12-03 21:53:00 +0100

    More radical cleanup

--- test/manifest/index.js
+++ test/manifest/index.js
@@ -1,60 +1,53 @@
 
 var manifest = require('../../manifest');
 
-require('colors');
-
 var diff = require('diff');
 var should = require('should');
 
 var path = require('path');
 var fs = require('fs');
-var readFile = fs.readFileSync;
-var readdir = fs.readdirSync;
-
-var fixturesDir = path.resolve(__dirname, 'fixtures');
-
-var cfgs = readdir(fixturesDir).sort().map(function (filename) {
-	filename = path.resolve(fixturesDir, filename);
-	return {
-		filename: path.relative(__dirname, filename),
-		fullFilename: filename,
-		package: require(filename)
-	};
-});
 
 describe("Cases:", function () {
-	cfgs.forEach(function (cfg) {
-		it(cfg.filename, function () {
-			var invalidPackage = cfg.filename.indexOf('INVALID') >= 0;
+	var fixturesDir = path.resolve(__dirname, 'fixtures');
+
+	fs.readdirSync(fixturesDir).forEach(function (filename) {
+		var fullname = path.resolve(fixturesDir, filename);
+		var invalidPackage = filename.indexOf('INVALID') >= 0;
+
+		it(filename, function () {
+			var pkg = JSON.parse(fs.readFileSync(fullname, 'utf8'));
+
+			// Validation
 
 			if (invalidPackage) {
-				(function () {
-					build(cfg.filename, cfg.fullFilename, cfg.package, invalidPackage);
-				}).should.throw();
+				validate.should.throw();
+				return;
 			}
 			else {
-				build(cfg.filename, cfg.fullFilename, cfg.package, invalidPackage);
+				validate();
+			}
+
+			// Building
+
+			build(filename, fullname, pkg);
+
+			function validate() {
+				manifest.validatePackage(pkg);
 			}
 		});
 	});
 });
 
-function build(filename, fullFilename, pkg, invalidPackage) {
-
-	manifest.validatePackage(pkg);
-
-	if (invalidPackage) {
-		return;
-	}
-
-	var expectedFilename = fullFilename
+function build(filename, fullname, pkg) {
+	var expectedFilename = fullname
 	.replace('fixtures', 'expected')
 	.replace('package', 'manifest')
 	.replace('.json', '');
 
 	var result = manifest.buildFromPackage(pkg).trim();
 
-	var expected = readFile(expectedFilename, 'utf8').trim()
+	var expected = fs.readFileSync(expectedFilename, 'utf8')
+	.trim()
 	.replace('$YEAR$', new Date().getFullYear());
 
 	var resultLines = result.split('\n');
@@ -63,55 +56,4 @@ function build(filename, fullFilename, pkg, invalidPackage) {
 	resultLines.forEach(function (line, i) {
 		line.should.be.eql(expectedLines[i]);
 	});
-
-	/*var err;
-
-	if (result !== expected) {
-
-		err = new Error("Wrong results for " + filename);
-		err.data = [];
-
-		err.data.push("\n");
-		err.data.push(filename.green + "\n");
-		err.data.push(path.relative(__dirname, expectedFilename).red + "\n");
-		err.data.push("\n");
-
-		diff.diffLines(expected, result).forEach(function(part){
-			// green for additions, red for deletions
-			// grey for common parts
-			var color =
-			part.added ? 'green' :
-			part.removed ? 'red' :
-			'white';
-
-			if (part.added || part.removed) {
-				err.data.push(expose(part.value, color));
-			}
-			else {
-				err.data.push(part.value[color]);
-			}
-		});
-
-		err.data.push("\n");
-		err.data.push("\n");
-
-		throw err;
-	}*/
-}
-
-function expose(line, color) {
-	line = replace(line, ' ', '·', color);
-	line = replace(line, '\t', '----', color);
-	return line;
-}
-
-function replace(line, from, to, color) {
-	return line.split(from).map(function (piece, index) {
-		if (index) {
-			return to.grey + piece[color];
-		}
-		else {
-			return piece[color];
-		}
-	}).join('');
 }
